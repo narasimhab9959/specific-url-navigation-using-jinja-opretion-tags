df4a9a1d5b48f2dcb4e2d5c9822cbc35f5e0db29
narasimhab9959 <narasimhab8555@gmail.com>
2023-06-01 18:21:38 +0530

    first commit

---
+++ mi/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path 
+from mi.views import * 
+app_name='mi'
+ 
+urlpatterns=[
+     path('rohit/',rohit,name='rohit'),
+]
---
+++ mi/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def rohit(request):
+    return render(request,'rohit.html')
+
+
+
+    
